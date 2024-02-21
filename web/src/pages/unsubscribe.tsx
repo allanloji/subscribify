6545d06f1ab09dd8d46e873155c4da351f1c3cb7
Allan Lopez <allanloji@gmail.com>
2024-02-21 17:27:43 -0600

    update readme

--- web/src/pages/unsubscribe.tsx
+++ web/src/pages/unsubscribe.tsx
@@ -1,8 +1,16 @@
 import Unsubscribe from "@/components/Unsubscribe";
+import Head from "next/head";
 
 export default function UnsusbcribePage() {
   return (
     <>
+      <Head>
+        <title>Unsubscribe from newsletter</title>
+        <meta
+          name="description"
+          content="Subscribify - Unsubscribe newsletter"
+        />
+      </Head>
       <Unsubscribe />
     </>
   );
